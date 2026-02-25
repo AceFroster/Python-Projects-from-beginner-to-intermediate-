ed59b7ea700ac50f3c9307ac1b207f98812ba9da
Jaat Devrajsingh <144952825+AceFroster@users.noreply.github.com>
2026-02-25 20:07:35 +0530

    Add simple band name generator functionality

--- Project 1/task.py
+++ Project 1/task.py
@@ -1,6 +1,11 @@
+# A Really Simple Band name Generator
+
+# Prints Greetings
 print("Greetings! User, I see that you're here to create a name for your band")
 
+# Takes User input
 city = input("What city did you grow up in? \n")
 pet = input("What pet do you like? \n")
 
+# Prints the band name
 print("Your band name is " + city + " " + pet)
