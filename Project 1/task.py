be62aae635acc58e208510acb6c5b76f10dabe4e
Jaat Devrajsingh <144952825+AceFroster@users.noreply.github.com>
2026-02-25 20:05:13 +0530

    Add files via upload

---
+++ Project 1/task.py
@@ -0,0 +1,6 @@
+print("Greetings! User, I see that you're here to create a name for your band")
+
+city = input("What city did you grow up in? \n")
+pet = input("What pet do you like? \n")
+
+print("Your band name is " + city + " " + pet)
