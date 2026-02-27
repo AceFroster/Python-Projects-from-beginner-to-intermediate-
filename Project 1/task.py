8bda33e7587b949dec07a359f1ae8a65c1f7ac35
Jaat Devrajsingh <144952825+AceFroster@users.noreply.github.com>
2026-02-27 18:46:55 +0530

    Update pet input prompt for clarity

--- Project 1/task.py
+++ Project 1/task.py
@@ -5,7 +5,7 @@ print("Greetings! User, I see that you're here to create a name for your band")
 
 # Takes User input
 city = input("What city did you grow up in? \n")
-pet = input("What pet do you like? \n")
+pet = input("What is the name of your pet \n")
 
 # Prints the band name
 print("Your band name is " + city + " " + pet)
